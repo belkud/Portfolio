e47602a3e8631cd99cbe7cf65eed8a13a56bf622
belkud <vlad-belkud@yandex.ru>
2023-10-02 19:24:21 +0300

    sdfdf

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,4 @@
 import './style.css'
 
 
-
-let button = document.querySelector('.btn')
-button.addEventListener('click', function(){
-  console.log('55555');
-  
-})
-
-let x =document.querySelector('.coordinate') as HTMLDivElement
-document.addEventListener ('mousemove', function(event){
-  x.innerHTML= `координаты окна по Х ${event.clientX} <br> координаты окна по У ${event.clientY}`
-})
+ 
