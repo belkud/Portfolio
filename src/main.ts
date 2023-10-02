00f9e0bb315eb64b6985b8d3d05175eecab6f8cb
belkud <vlad-belkud@yandex.ru>
2023-10-03 00:06:42 +0300

    kjkllhh

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,10 @@
 import './style.css'
 
 
+let mouseCoordinate = document.querySelector('.container') as HTMLDivElement
+mouseCoordinate.addEventListener ('click', function(event){
+    innerHTML(`${event.pageY}`);
+
+})
+
  
