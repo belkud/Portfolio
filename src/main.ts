0e02cfba3b00056838aac4040faa945f94159684
belkud <vlad-belkud@yandex.ru>
2023-10-03 19:08:47 +0300

    ada

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,6 @@
 import './style.css'
 
 
-let scroll = document.querySelectorAll('.navbar') 
-scroll.forEach(navbar=>
-    navbar.addEventListener ('click', function(event){
-    console.log(`${event.pageY}`);
 
-}))
 
- 
+
