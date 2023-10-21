c5663dba411472b2c69fb3e2044084432ddf1e87
belkud <vlad-belkud@yandex.ru>
2023-10-21 12:48:05 +0300

    sfdd

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,7 @@ import './style.css'
 
 
 
-
+let battery = document.querySelector('.imgbattery') as HTMLDivElement
+// let y = 150+'px'
+battery.style.background = 'yellow'
+// battery.style.transition = 5+'s'
