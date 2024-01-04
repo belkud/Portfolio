f3ad8308b6273a483c14ee00a7f83e22acb64dbf
belkud <vlad-belkud@yandex.ru>
2024-01-05 00:57:44 +0300

    dfgf

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,12 @@ import './style.css'
 
  
 // ! ящик с шириной экрана (пиксели)
-const width_of_display = document.querySelector ('#width_of_display') as HTMLButtonElement
-window.addEventListener ('resize', ()=> {
-  let computedStyle = getComputedStyle(document.body)
-  width_of_display.innerHTML = 'ширина экрана ' + computedStyle.width 
+// const width_of_display = document.querySelector ('#width_of_display') as HTMLButtonElement
+// window.addEventListener ('resize', ()=> {
+//   let computedStyle = getComputedStyle(document.body)
+//   width_of_display.innerHTML = 'ширина экрана ' + computedStyle.width 
   
-})
+// })
 
 
 
@@ -33,10 +33,22 @@ const observer = new IntersectionObserver((entries, observer)=>{
 battery.forEach((el)=>observer.observe(el))
 
 
+const carBlock = document.querySelector('#carBlock') as HTMLDivElement
+const car = document.querySelector('#car') as HTMLImageElement
 const car2 = document.querySelector('#car2') as HTMLImageElement
+carBlock.addEventListener('mousemove', ()=> {
+  car.style.filter = `drop-shadow(10px 10px 2px black)`
+  car.style.transition = .3 + 's'
+})
+
+carBlock.addEventListener('mouseout', ()=> {
+  car.style.filter = ''
+})
+
+
 setInterval(()=> {
-car2.style.display = 'block'
+car2.style.opacity = '0'
 },1000)
 setInterval(()=> {
-car2.style.display = 'none'
+car2.style.opacity = '1'
 },2000)
